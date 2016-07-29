f831389d1e0aae61d49c9cf83a2dbf10b7d4b0b5
Cradanimal <cradwimb@gmail.com>
2016-07-29 13:38:21 -0700

    (feat) build out CompletedView component

---
+++ client/completedView.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  AppRegistry, 
+  ScrollView,
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+import { CompletedDiv } from './completedTasks.js';
+
+export default class CompletedView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completedTaskList : this.props.completedTaskList
+    };
+  }
+
+  render() {
+    return (
+      <View style={style.container}>
+        <Text style={style.title} >Completed Tasks</Text>
+        <ScrollView automaticallyAdjustContentInsets={false} style={style.scrollView}>{
+          this.state.completedTaskList.map( comTask => {
+            return (<CompletedDiv  key={comTask.id} task={comTask.task} user={comTask.user} />)}
+            )
+          }
+        </ScrollView>
+      </View>
+      );
+  }
+};
+
+
+const style = {
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30
+  },
+  scrollView: {
+     height: 400,
+   }
+};
